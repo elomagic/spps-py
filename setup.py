bf3f74145b857a1d7773f237b8a1cdbf75764236
Carsten Rambow <dev@elomagic.de>
2021-02-15 16:38:19 +0100

    Project structure updated

--- setup.py
+++ setup.py
@@ -11,20 +11,21 @@ __license__ = "Apache-2.0"
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license_text = f.read()
-
 setup(
     name="spps",
-    version="1.0.0",
+    version="1.0.0rc1",
     description="Simple Password Protection Solution for Python",
-    long_description="file:readme",
+    long_description=readme,
     long_description_content_type="text/markdown",
     author="Carsten Rambow",
     author_email="spps.dev@elomagic.de",
     url="https://github.com/elomagic/spps-py",
+    project_urls={
+        'Source': 'https://github.com/elomagic/spps-py',
+        'Tracker': 'https://github.com/elomagic/spps-py/issues'
+    },
     license="Apache-2.0",
     python_requires=">=3.9",
-    keywords=["encrypt", "decrypt", "password", "security", "hide", "protect", "key", "secret"],
+    keywords=["encrypt", "decrypt", "password", "security", "hide", "protect", "key", "secret", "AES", "GCM"],
     packages=find_packages(exclude=('tests', 'docs'))
 )
